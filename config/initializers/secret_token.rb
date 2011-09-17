ef44e5ec3061f3116d615ae19b08ec0eda0a7479
yumi.ueda <ueda0820@gmail.com>
2011-09-17 16:06:07 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karaage::Application.config.secret_token = '45e09f26e80790ea9187802862058f828e37424f70b8f792507a997dfd842ce05ecf89b42319c01ddf04aaf67f7e35b8aed9b0f296b085b6fde111cff3bb2515'
